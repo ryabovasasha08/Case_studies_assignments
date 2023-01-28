48d0b1676859afdf27d228f41f02c9a8238e4791
Alexandra Ryabova <alexandra-ryabova@stud.onu.edu.ua>
2023-01-28 22:19:54 +0200

    Generate images centered, without padding and rotation

--- generate/characters_generate.py
+++ generate/characters_generate.py
@@ -10,44 +10,51 @@ def generate_characters(num_samples_per_character):
 
     for num in range(0, 10):
         for i in range(num_samples_per_character):
-            img = Image.new('RGB', (15, 20), color=(255, 255, 255))
+            W = 10
+            H = 16
+            img = Image.new('RGB', (W, H), color=(255, 255, 255))
             d = ImageDraw.Draw(img)
 
             fontsize = 20
-            font = ImageFont.truetype("generate/EuroPlate.ttf",fontsize)
+            font = ImageFont.truetype("generate/EuroPlate.ttf", fontsize)
 
-            d.text((0, 0), str(num), font=font, fill=(0, 0, 0), align="center")
-            img = img.rotate(random.randint(-10, 10), expand=True, fillcolor=(255, 255, 255))
+            _, _, w, h = d.textbbox((0, 0), str(num), font=font)
+
+            d.text(((W - w) / 2, (H - h) / 2), str(num), font=font, fill=(0, 0, 0), align="center")
+            # img = img.rotate(random.randint(-10, 10), expand=True, fillcolor=(255, 255, 255))
             img = img.resize((15, 20))
 
             name = str(num) + "_" + str(i) + ".png"
             img.save("database/characters/" + name)
 
     for i in range(num_samples_per_character):
-        img = Image.new('RGB', (15, 20), color=(255, 255, 255))
+        W = 10
+        H = 16
+        img = Image.new('RGB', (W, H), color=(255, 255, 255))
         d = ImageDraw.Draw(img)
 
         fontsize = 20
         font = ImageFont.truetype("generate/EuroPlate.ttf", fontsize)
-
-        d.text((0, 0), "-", font=font, fill=(0, 0, 0), align="center")
-        img = img.rotate(random.randint(-10, 10), expand=True, fillcolor=(255, 255, 255))
+        _, _, w, h = d.textbbox((0, 0), "-", font=font)
+        d.text(((W - w) / 2, (H - h) / 2), "-", font=font, fill=(0, 0, 0), align="center")
+        # img = img.rotate(random.randint(-10, 10), expand=True, fillcolor=(255, 255, 255))
         img = img.resize((15, 20))
 
         name = "AA" + "_" + str(i) + ".png"
         img.save("database/characters/" + name)
 
     for let in string.ascii_uppercase:
-
         for i in range(num_samples_per_character):
-            img = Image.new('RGB', (15, 10), color=(255, 255, 255))
+            W = 10
+            H = 16
+            img = Image.new('RGB', (W, H), color=(255, 255, 255))
             d = ImageDraw.Draw(img)
-
+            _, _, w, h = d.textbbox((0, 0), let, font=font)
             fontsize = 20
             font = ImageFont.truetype("generate/EuroPlate.ttf", fontsize)
 
-            d.text((0, 0), let, font=font, fill=(0, 0, 0), align="center")
-            img = img.rotate(random.randint(-10, 10), expand=True, fillcolor=(255, 255, 255))
+            d.text(((W - w) / 2, (H - h) / 2), let, font=font, fill=(0, 0, 0), align="center")
+            # img = img.rotate(random.randint(-10, 10), expand=True, fillcolor=(255, 255, 255))
             img = img.resize((15, 20))
 
             name = let + "_" + str(i) + ".png"
