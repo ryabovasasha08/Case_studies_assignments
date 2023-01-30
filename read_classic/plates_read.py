remove_overlapping_bounding_rects in the characters segmentation; Fix order of h, w in some places; try to check variance to recogize dashes (not working); add metrics to calculate the accuracies

read_classic/plates_read.py
@@ -7,14 +7,15 @@ import os
 import numpy as np
 from datetime import datetime
 from PIL import Image, ImageDraw, ImageFont, ImageOps
+from scipy import ndimage
 import string
+import metrics
 import random
 import cv2
 import re
 import os
 import numpy as np
 from matplotlib import cm
-
 from generate.characters_generate import generate_characters
 from generate.plates_generate import create_plates
 from read_classic.train_model_characters import create_and_train_model, convert_to_text
@@ -84,16 +85,42 @@ def get_box_points(x, y, w, h, alpha):
     return cv2.boxPoints(((x, y), (w, h), alpha))
 
 
+# Fix the wrong formula (now rects are rotated, so formula for x2, y2 is wrong)
+def remove_overlapping_bounding_rects(bounding_rects, overlap_percent):
+    overlapped_indices = []
+    max_idx = len(bounding_rects)
+    for chosen_rect_idx in range(max_idx):
+        x1, y1, h1, w1, alpha1 = bounding_rects[chosen_rect_idx]
+        area1 = w1 * h1
+        for j in range(chosen_rect_idx + 1, max_idx):
+            if chosen_rect_idx != j:
+                x2, y2, h2, w2, alpha2 = bounding_rects[j]
+                x_overlap = max(0, min(x1 + w1, x2 + w2) - max(x1, x2))
+                y_overlap = max(0, min(y1 + h1, y2 + h2) - max(y1, y2))
+                area_overlap = x_overlap * y_overlap
+                area2 = w2 * h2
+                if area_overlap > overlap_percent * min(area1, area2):
+                    if area1 < area2:
+                        overlapped_indices.append(chosen_rect_idx)
+                    else:
+                        overlapped_indices.append(j)
+    k = len(bounding_rects)
+    overlapped_indices = list(dict.fromkeys(overlapped_indices))
+    np_output = np.array(bounding_rects)
+    np_output = np.delete(np_output, overlapped_indices, 0)
+    return np_output.tolist()
+
+
 # Get a segment of size (15, 20) - the same size as characters in database
 def get_square_segment(x, y, w, h, alpha, size, gray, i):
     box = get_box_points(x, y, w, h, alpha)
     box = np.int0(box)
-    print(box)
+    # print(box)
     # the order of the box points: first the lowest one, and then clockwise from there.
     # So it can be: bottom left, top left, top right, bottom right
     # Or it can be: bottom right, bottom left, top left, top right
     # Check which case is that now:
-    print(alpha)
+    # print(alpha)
     if alpha >= 45:
         last_box_point = box[3].copy()
         box[3] = box[2]
@@ -112,11 +139,17 @@ def get_square_segment(x, y, w, h, alpha, size, gray, i):
     warped = warped.resize((size - 5, size))
     warped = np.array(warped)
 
-    cv2.imshow("warped" + str(i), warped)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # cv2.imshow("warped" + str(i), warped)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
     return warped
+
+
+# 9000 is empirical value - only 8 sometines doesn't satisfy
+def is_segment_dash(img):
+    print(ndimage.variance(img))
+    return ndimage.variance(img) < 9000
 
 
 def detect_lp(gray, text):
@@ -136,25 +169,31 @@ def detect_lp(gray, text):
     bounding_rects = []
     for cnt in contours:
         # Get rectangle bounding contour
-        (x, y), (w, h), alpha = cv2.minAreaRect(cnt)
+        (x, y), (h, w), alpha = cv2.minAreaRect(cnt)
 
         # Don't plot small false positives that aren't license plates
-        if w > 40 or h > 40 or w < 8 or y < 8:
+        # Height can be really small though (in case of dash)
+        if w > 35 or h > 35 or w < 8 or h < 5:
             continue
-        bounding_rects.append([x, y, w, h, alpha])
+        bounding_rects.append([x, y, h, w, alpha])
 
     '''Removing letter D as the most left rectangle and all the defined rectangles that overlap with it'''
     most_left_rect = min(bounding_rects, key=lambda t: t[0])
     bounding_rects.remove(most_left_rect)
-    chosen_x1, chosen_y1, chosen_w, chosen_h, chosen_alpha = most_left_rect
+    chosen_x1, chosen_y1, chosen_h, chosen_w, chosen_alpha = most_left_rect
     # Fix the wrong formula (now rects are rotated, so formula for x2, y2 is wrong)
     chosen_x2, chosen_y2 = chosen_x1 + chosen_w, chosen_y1 + chosen_h
     for rect in bounding_rects:
-        x1, y1, w, h, alpha = rect
+        x1, y1, h, w, alpha = rect
         x2, y2 = x1 + w, y1 + h
         if (x1 < chosen_x2 and x2 > chosen_x1 and
                 y1 < chosen_y2 and y2 > chosen_y1):
             bounding_rects.remove(rect)
+
+    # print(len(bounding_rects))
+    '''Remove overlapping bounding rectangles'''
+    bounding_rects = remove_overlapping_bounding_rects(bounding_rects, 0.5)
+    # print(len(bounding_rects))
 
     '''Check if the license plate is 1-liner or 2-liners'''
     is_one_line = True
@@ -187,31 +226,35 @@ def detect_lp(gray, text):
         bounding_rects = first_line
 
     ''' Convert all the segments in the separate images of the correct size (15*20 - same as chars in database)'''
-    img_list = [get_square_segment(x, y, w, h, alpha, 20, gray, i) for i, [x, y, w, h, alpha] in
+    img_list = [get_square_segment(x, y, h, w, alpha, 20, gray, i) for i, [x, y, h, w, alpha] in
                 enumerate(bounding_rects)]
 
     predicted_text = convert_to_text(img_list)
 
     '''If the bounding rectangle is too wide and short- it is probably a dash'''
-    for i, [x, y, w, h, alpha] in enumerate(bounding_rects):
-        if w > h * 2:
+    for i, [x, y, h, w, alpha] in enumerate(bounding_rects):
+        if w > h * 2 and i != 0 and i != len(bounding_rects) - 1:
             predicted_text[i] = "-"
 
-    print("Real text: ", text)
-    print("Predicted text: ", predicted_text)
+    '''Try check variance of segment to recognize dashes'''
+    # for i, img in enumerate(img_list):
+    #     if is_segment_dash(img):
+    #         predicted_text[i] = "-"
+
+    rgb_image = np.repeat(gray[:, :, np.newaxis], 3, axis=2)
 
     '''Draw the rectangles around the characters in license plate'''
-    for [x, y, w, h, alpha] in bounding_rects:
-        box = get_box_points(x, y, w, h, alpha)
+    for [x, y, h, w, alpha] in bounding_rects:
+        box = get_box_points(x, y, h, w, alpha)
         box = np.int0(box)
-        cv2.drawContours(gray, [box], 0, (150, 150, 150), 1)
+        cv2.drawContours(rgb_image, [box], 0, (0, 0, 255), 1)
 
-    return gray
+    return rgb_image, "".join(predicted_text)
 
 
 # Number of plates (right now in 'plates' folder there's 7k images
 N = 100
-# generate_characters(1)
+#generate_characters(N)
 # create_plates(N)  # to generate a folder 'plates' with N images and masks with N images
 # model = create_and_train_model()
 
@@ -219,9 +262,33 @@ images_dict = load_bw_images_dict_from_folder("database/plates")
 images = list(images_dict.values())
 images_platenames = list(images_dict.keys())
 
+test_size = 100
+avg_percent_of_correct_placed_chars = 0
+avg_length_identical = 0
+avg_text_identical = 0
+
 for i, image in enumerate(images):
-    cv2.imshow(images_platenames[i], detect_lp(image, images_platenames[i]))
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    if i > 10:
+    img_with_contours, predicted_text = detect_lp(image, images_platenames[i])
+    text = images_platenames[i][0:images_platenames[i].find('.')]
+
+    print("Image number ", i)
+    print("Real text: ", text)
+    print("Predicted text: ", predicted_text)
+
+    avg_percent_of_correct_placed_chars += metrics.get_correct_placed_chars_percent(text, predicted_text)
+    avg_length_identical += metrics.is_length_same(text, predicted_text)
+    avg_text_identical +=  metrics.is_text_same(text, predicted_text)
+
+    # cv2.imshow(images_platenames[i], img_with_contours)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    #
+    if i > 100:
         break
+
+avg_percent_of_correct_placed_chars /= test_size
+avg_length_identical /= test_size
+avg_text_identical /= test_size
+print("avg_percent_of_correct_placed_chars: ", avg_percent_of_correct_placed_chars)
+print("avg_length_identical: ", avg_length_identical)
+print("avg_text_identical: ", avg_text_identical)